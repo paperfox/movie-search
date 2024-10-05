6986ee4c55d1097d65aa418a160921baf11d2bea
paperfox <ndg.paperfox@gmail.com>
2024-10-05 11:04:27 -0400

    Animation - adding animation to transitions

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import { useState, useEffect } from 'react';
-import ListItems from './components/ListItems';
-import Footer from './components/Footer';
-import ListGroup from 'react-bootstrap/ListGroup';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
-import projector from './assets/projector.svg';
-import popcornPiece1 from './assets/popcorn-1.svg';
-// import popcornPiece2 from './assets/popcorn-2.svg';
-// import popcornPiece3 from './assets/popcorn-3.svg';
+import { useState, useCallback, useEffect } from "react";
+import ListItems from "./components/ListItems";
+import Footer from "./components/Footer";
+import ListGroup from "react-bootstrap/ListGroup";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import projector from "./assets/projector.svg";
+import { useDebounce } from "use-debounce";
 
 interface MovieData {
   id: number;
@@ -25,21 +23,24 @@ interface MovieData {
 
 function App() {
   const [movies, setMovies] = useState<MovieData[]>([]);
-  const [term, setTerm] = useState('');
+  const [term, setTerm] = useState("");
   const apiKey = import.meta.env.VITE_KEY;
   const url = `https://api.themoviedb.org/3/search/movie?query=${term}&include_adult=false&language=en-US&page=1`;
 
+  const [debouncedTerm] = useDebounce(term, 1000);
+  console.log("search term:", debouncedTerm);
+
   useEffect(() => {
     const fetchData = async () => {
-      if (!term) {
-        return console.log('No search term provided');
+      if (!debouncedTerm) {
+        return console.log("No search term provided");
       }
 
       try {
         const options = {
-          method: 'GET',
+          method: "GET",
           headers: {
-            accept: 'application/json',
+            accept: "application/json",
             Authorization: `Bearer ${apiKey}`,
           },
         };
@@ -56,81 +57,100 @@ function App() {
       }
     };
     fetchData();
-  }, [term]);
+  }, [debouncedTerm]);
+
+  const clearSearch = useCallback(() => {
+    setTerm(" ");
+  }, []);
 
   return (
     <>
-      {movies.length > 0 ? (
-        <>
-          <header className="as-nav">
-            <div className="row justify-content-center">
-              <div className="col-lg-7 col-xl-6">
-                <h1 className="page-title">Find the Perfect Movie</h1>
-
-                <div className="row justify-content-center m-4">
-                  <div className="col-auto align-self-center">
-                    <label htmlFor="input-movie-search">Search for movies:</label>
-                  </div>
-                  <div className="col-sm-8">
-                    <InputGroup>
-                      <Form.Control
-                        id="input-movie-search"
-                        aria-describedby="btn-movie-search"
-                        value={term}
-                        type="text"
-                        onChange={(e) => setTerm(e.target.value)}
-                      />
-                      <Button variant="primary" id="btn-movie-search" aria-label="Search">
-                        <span className="icon-search"></span>
-                      </Button>
-                    </InputGroup>
-                  </div>
-                </div>
+      <header
+        className={
+          movies.length === 0 ? "container-fluid full-window" : "as-nav"
+        }
+      >
+        <div>
+          <div
+            className={
+              movies.length === 0
+                ? "animate-width landing-content"
+                : "animate-width nav-width"
+            }
+          >
+            <h1 className="page-title">Find the Perfect Movie</h1>
+            <form className="row my-4">
+              <div className="col-auto align-self-center">
+                <label htmlFor="input-movie-search">Search for movies:</label>
               </div>
-            </div>
-          </header>
-
-          <main className="container">
-            <img src={popcornPiece1} className="knocked-popcorn" alt="" />
-            <div className="row justify-content-center">
-              <div className="col-auto col-lg-10 col-xl-9 align-self-center">
-                <ListGroup>{movies.length > 0 ? movies.map((movie) => <ListItems movie={movie} />) : ''}</ListGroup>
-              </div>
-            </div>
-          </main>
-        </>
-      ) : (
-        <header className="container-fluid full-window">
-          <div className="row">
-            <div className="col-auto offset-lg-1 align-self-center landing-content">
-              <h1 className="page-title">Find the Perfect Movie</h1>
-              <div>
-                <label htmlFor="input-movie-search" className="d-block mb-2">
-                  Search for movies:
-                </label>
-                {/* <input id="input-movie-search" value={term} type="text" onChange={(e) => setTerm(e.target.value)} /> */}
-                <InputGroup className="mb-3">
+              <div className={movies.length > 0 ? "col-sm-6" : "col-sm-9"}>
+                <InputGroup>
                   <Form.Control
                     id="input-movie-search"
                     aria-describedby="btn-movie-search"
                     value={term}
-                    type="text"
+                    type="search"
                     onChange={(e) => setTerm(e.target.value)}
                   />
-                  <Button variant="primary" id="btn-movie-search" aria-label="Search">
+                  <Button
+                    variant="primary"
+                    id="btn-movie-search"
+                    aria-label="Search"
+                  >
                     <span className="icon-search"></span>
                   </Button>
                 </InputGroup>
               </div>
-            </div>
+
+              {movies.length > 0 ? (
+                <p className="col-auto align-self-center mb-0">
+                  <Button variant="link" type="button" onClick={clearSearch}>
+                    Back to Landing
+                  </Button>
+                </p>
+              ) : (
+                ""
+              )}
+            </form>
           </div>
-          <img src={projector} className="splash-image" alt="Image of a vintage movie projector" />
-          <div className="shape-left-top"></div>
-          <div className="shape-left-bottom"></div>
-          <div className="shape-right-top"></div>
-          <div className="shape-right-bottom"></div>
-        </header>
-      )}
+        </div>
+
+        {movies.length === 0 ? (
+          <>
+            <img
+              src={projector}
+              className="splash-image"
+              alt="Image of a vintage movie projector"
+            />
+            <div className="shape-left-top"></div>
+            <div className="shape-left-bottom"></div>
+            <div className="shape-right-top"></div>
+            <div className="shape-right-bottom"></div>
+          </>
+        ) : (
+          ""
+        )}
+      </header>
+
+      <main className="container">
+        {movies.length > 0 ? (
+          <>
+            <div className="row justify-content-center">
+              <div className="col-auto col-lg-10 col-xl-9 align-self-center">
+                <ListGroup>
+                  {movies.length > 0
+                    ? movies.map((movie) => <ListItems movie={movie} />)
+                    : ""}
+                </ListGroup>
+              </div>
+            </div>
+          </>
+        ) : (
+          <p className="visually-hidden">
+            No movies yet! Use the search field to get a list.
+          </p>
+        )}
+      </main>
       <Footer />
     </>
   );
