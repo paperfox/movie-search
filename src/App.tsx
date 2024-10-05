66b72fe052feb8e0d32ada7f404bab2f236c5d70
paperfox <ndg.paperfox@gmail.com>
2024-10-05 12:45:20 -0400

    Clean up - better organization and reusability, added comments, deleted unused things

--- src/App.tsx
+++ src/App.tsx
@@ -24,18 +24,18 @@ interface MovieData {
 function App() {
   const [movies, setMovies] = useState<MovieData[]>([]);
   const [term, setTerm] = useState("");
+
   const apiKey = import.meta.env.VITE_KEY;
   const url = `https://api.themoviedb.org/3/search/movie?query=${term}&include_adult=false&language=en-US&page=1`;
 
   const [debouncedTerm] = useDebounce(term, 1000);
-  console.log("search term:", debouncedTerm);
 
+  // gets movies when user pauses in typing
   useEffect(() => {
     const fetchData = async () => {
       if (!debouncedTerm) {
         return console.log("No search term provided");
       }
-
       try {
         const options = {
           method: "GET",
@@ -44,30 +44,30 @@ function App() {
             Authorization: `Bearer ${apiKey}`,
           },
         };
-        // console.log('Fetching data from API...');
         const response = await fetch(url, options);
-        // if (!response.ok) {
-        //   throw new Error(`HTTP error! status: ${response.status}`);
-        // }
         const data = await response.json();
-        // console.log('Data received:', data);
         setMovies(data.results);
       } catch (error) {
-        // console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       }
     };
     fetchData();
   }, [debouncedTerm]);
 
+  // sets search term to empty string which reloads the landing page
   const clearSearch = useCallback(() => {
     setTerm(" ");
   }, []);
 
+  // settings the classes based on the length of the movies array below ensures the focus doesn't leave the search box
+  // this maintains a consistent user experience as the user goes from one layout to the next since it's a single page app
   return (
     <>
       <header
         className={
-          movies.length === 0 ? "container-fluid full-window" : "as-nav"
+          movies.length === 0
+            ? "container-fluid landing-page"
+            : "list-navigation"
         }
       >
         <div>
@@ -75,30 +75,31 @@ function App() {
             className={
               movies.length === 0
                 ? "animate-width landing-content"
-                : "animate-width nav-width"
+                : "animate-width search-navigation"
             }
           >
             <h1 className="page-title">Find the Perfect Movie</h1>
             <form className="row my-4">
-              <div className="col-auto align-self-center">
-                <label htmlFor="input-movie-search">Search for movies:</label>
+              <div
+                className={
+                  movies.length === 0
+                    ? "col-sm-4"
+                    : "col-auto align-self-center"
+                }
+              >
+                <label htmlFor="input-movie-search">Search</label>
               </div>
-              <div className={movies.length > 0 ? "col-sm-6" : "col-sm-9"}>
+              <div className={movies.length === 0 ? "col-sm-9" : "col-sm-6"}>
                 <InputGroup>
                   <Form.Control
                     id="input-movie-search"
-                    aria-describedby="btn-movie-search"
                     value={term}
                     type="search"
                     onChange={(e) => setTerm(e.target.value)}
                   />
-                  <Button
-                    variant="primary"
-                    id="btn-movie-search"
-                    aria-label="Search"
-                  >
+                  <InputGroup.Text>
                     <span className="icon-search"></span>
-                  </Button>
+                  </InputGroup.Text>
                 </InputGroup>
               </div>
 
@@ -139,7 +140,9 @@ function App() {
               <div className="col-auto col-lg-10 col-xl-9 align-self-center">
                 <ListGroup>
                   {movies.length > 0
-                    ? movies.map((movie) => <ListItems movie={movie} />)
+                    ? movies.map((movie) => (
+                        <ListItems movie={movie} key={movie.id} />
+                      ))
                     : ""}
                 </ListGroup>
               </div>
--- src/components/ListItems.tsx
+++ src/components/ListItems.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react';
-import Modal from 'react-bootstrap/Modal';
-import ListGroup from 'react-bootstrap/ListGroup';
-import { Button } from 'react-bootstrap';
-import popcorn from '../assets/popcorn.svg';
+import { useState } from "react";
+import Modal from "react-bootstrap/Modal";
+import ListGroup from "react-bootstrap/ListGroup";
+import { Button } from "react-bootstrap";
+import popcorn from "../assets/popcorn.svg";
 
 interface movieItem {
   id: number;
@@ -20,11 +20,20 @@ interface movieItem {
 function ListItems({ movie }: { movie: movieItem }) {
   const [show, setShow] = useState(false);
 
-  const date = new Date(movie.release_date + 'T00:00:00').toLocaleDateString();
-  const truncate = (input: string) => (input.length > 130 ? `${input.substring(0, 130)}...` : input);
-  const shortOverview = truncate(movie.overview);
-  const poster = movie.poster_path === null ? popcorn : `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+  const date = new Date(movie.release_date + "T00:00:00").toLocaleDateString();
 
+  // Truncate movie overview to a reasonanble length, users can click the list item to view details to see the full overview
+  const truncate = (input: string) =>
+    input.length > 130 ? `${input.substring(0, 130)}...` : input;
+  const shortOverview = truncate(movie.overview);
+
+  // poster preview, for visual interest and to get folks to click into the ddetial view to see more
+  const poster =
+    movie.poster_path === null
+      ? popcorn
+      : `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+
+  // Modal the BS5 modal, styled with custom animation
   const handleShowModal = () => {
     setShow(!show);
   };
@@ -36,7 +45,7 @@ function ListItems({ movie }: { movie: movieItem }) {
           <div className="col-3 col-sm-2 col-md-1">
             <div className="img-container">
               <div className="img-overlay"></div>
-              <img src={poster} alt={`Movie poster for ${movie.title}`} className="img-fluid" />
+              <img src={poster} alt="" className="img-fluid" />
             </div>
           </div>
           <div className="col-9 col-sm-3">
@@ -60,7 +69,11 @@ function ListItems({ movie }: { movie: movieItem }) {
         <Modal.Body>
           <div className="row justify-content-center m-5">
             <div className="col-9 col-sm-6 col-md-5 col-lg-4 col-xl-3 pb-4">
-              <img className="img-fluid pe-3" src={poster} alt={`Movie poster for ${movie.title}`} />
+              <img
+                className="img-fluid pe-3"
+                src={poster}
+                alt={`Movie poster for ${movie.title}`}
+              />
             </div>
             <div className="col-sm-6 col-lg-6 col-xl-5 mt-4">
               <h2>{movie.title}</h2>
@@ -84,20 +97,17 @@ function ListItems({ movie }: { movie: movieItem }) {
               <p>
                 <strong>Vote Count:</strong> {movie.vote_count}
               </p>
-              <div>
+              {/* <div>
                 <span className="badge rounded-pill text-bg-light">GENRE</span>
                 <span className="badge rounded-pill text-bg-light">GENRE</span>
                 <span className="badge rounded-pill text-bg-light">GENRE</span>
-              </div>
+              </div> */}
             </div>
           </div>
           {/* "genre_ids": [
       27,
       35
   ], */}
-          {/* <p className="text-center">
-            Popularity: {movie.popularity} | Vote Average: {movie.vote_average} | Vote Count: {movie.vote_count}
-          </p> */}
           <div className="text-center mt-5 mb-3">
             <Button variant="primary" onClick={handleShowModal}>
               CLOSE
